03faaece848ce15973bb064e234fcaf60db54ad8
Brinley Developpment <121724762+brinleydev@users.noreply.github.com>
2024-11-19 19:31:23 +0100

    Create ptfx.lua

---
+++ client/ptfx.lua
@@ -0,0 +1,69 @@
+function PtfxThis(asset)
+    while not HasNamedPtfxAssetLoaded(asset) do
+        RequestNamedPtfxAsset(asset)
+        Wait(10)
+    end
+    UseParticleFxAsset(asset)
+end
+
+function PtfxStart()
+    LocalPlayer.state:set('ptfx', true, true)
+end
+
+function PtfxStop()
+    LocalPlayer.state:set('ptfx', false, true)
+end
+
+AddStateBagChangeHandler('ptfx', nil, function(bagName, key, value, _unused, replicated)
+    local plyId = tonumber(bagName:gsub('player:', ''), 10)
+
+    -- We stop here if we don't need to go further
+    -- We don't need to start or stop the ptfx twice
+    if (PlayerParticles[plyId] and value) or (not PlayerParticles[plyId] and not value) then return end
+
+    -- Only allow ptfx change on players
+    local ply = GetPlayerFromServerId(plyId)
+    if ply == 0 then return end
+
+    local plyPed = GetPlayerPed(ply)
+    if not DoesEntityExist(plyPed) then return end
+
+    local stateBag = Player(plyId).state
+    if value then
+        -- Start ptfx
+
+        local asset = stateBag.ptfxAsset
+        local name = stateBag.ptfxName
+        local offset = stateBag.ptfxOffset
+        local rot = stateBag.ptfxRot
+        local boneIndex = stateBag.ptfxBone and GetPedBoneIndex(plyPed, stateBag.ptfxBone) or GetEntityBoneIndexByName(name, "VFX")
+        local scale = stateBag.ptfxScale or 1
+        local color = stateBag.ptfxColor
+        local propNet = stateBag.ptfxPropNet
+        local entityTarget = plyPed
+        -- Only do for valid obj
+        if propNet then
+            local propObj = NetToObj(propNet)
+            if DoesEntityExist(propObj) then
+                entityTarget = propObj
+            end
+        end
+        PtfxThis(asset)
+        PlayerParticles[plyId] = StartNetworkedParticleFxLoopedOnEntityBone(name, entityTarget, offset.x, offset.y, offset.z, rot.x, rot.y, rot.z, boneIndex, scale + 0.0, 0, 0, 0, 1065353216, 1065353216, 1065353216, 0)
+        if color then
+            if color[1] and type(color[1]) == 'table' then
+                local randomIndex = math.random(1, #color)
+                color = color[randomIndex]
+            end
+            SetParticleFxLoopedAlpha(PlayerParticles[plyId], color.A)
+            SetParticleFxLoopedColour(PlayerParticles[plyId], color.R / 255, color.G / 255, color.B / 255, false)
+        end
+        print("Started PTFX: " .. PlayerParticles[plyId])
+    else
+        -- Stop ptfx
+        print("Stopped PTFX: " .. PlayerParticles[plyId])
+        StopParticleFxLooped(PlayerParticles[plyId], false)
+        RemoveNamedPtfxAsset(stateBag.ptfxAsset)
+        PlayerParticles[plyId] = nil
+    end
+end)
