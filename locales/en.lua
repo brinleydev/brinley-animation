6aa76e661efdaa401a984fedc2570ab600c7e168
Brinley Developpment <121724762+brinleydev@users.noreply.github.com>
2024-11-19 19:43:52 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,17 @@
+if not Locales then Locales = {} end
+Locales['en'] = {
+    ['description'] = 'Click on the button to see the animation',
+    ['animations'] = 'Animations',
+    ['quick_animations'] = 'Quick Animations',
+
+    ['search_placeholder'] = 'Search...',
+    ['accept_shared_anim'] = 'Accept [Y]',
+    ['decline_shared_anim'] = 'Decline [H]',
+
+    ['notification_error'] = 'Error',
+    ['no_player_nearby'] = 'No players nearby!',
+
+    ['new_invite'] = 'New Invite!',
+    ['invited_animation'] = 'Invited %s animation',
+    ['invite_question'] = 'Do u want to accept?'
+}
