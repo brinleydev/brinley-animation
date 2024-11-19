4d26ad0d71f4184e3a099256bebad695b688a2fd
Brinley Developpment <121724762+brinleydev@users.noreply.github.com>
2024-11-19 19:44:30 +0100

    Create tr.lua

---
+++ locales/tr.lua
@@ -0,0 +1,13 @@
+if not Locales then Locales = {} end
+Locales['tr'] = {
+    ['description'] = 'Animasyonu görmek için butona tıklayın.',
+    ['animations'] = 'Animasyon',
+    ['quick_animations'] = 'Kısayol Animasyonlar',
+
+    ['search_placeholder'] = 'Ara...',
+    ['accept_shared_anim'] = 'Kabul Et [Y]',
+    ['decline_shared_anim'] = 'Reddet [H]',
+
+    ['notification_error'] = 'Hata',
+    ['no_player_nearby'] = 'Yakında hiçbir oyuncu yok!'
+}
